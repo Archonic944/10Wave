869d6ec097e99a18d6706078a34249dabd0ac9e3
Archonic <55026152+Archonic944@users.noreply.github.com>
2025-01-27 10:44:20 -0500

    Update high score

--- html/src/main/java/club/hack/painbox/gwt/GwtLauncher.java
+++ html/src/main/java/club/hack/painbox/gwt/GwtLauncher.java
@@ -48,7 +48,7 @@ public class GwtLauncher extends GwtApplication {
         d.setTitle("10Wave");
         BodyElement body = d.getBody();
         Element explanation = d.createElement("p");
-        explanation.setInnerHTML("Clear 10 waves of enemies as fast as possible! <strong>The current record is 42 seconds.</strong> <br> Use A and D to move, W and S to switch gravity (but you barely need to do that), and the arrow keys to shoot.<br><br>Shoot as much as you want, but be careful not to get hit by your own bullets...<br><br><strong>Please enable autoplay in your browser settings to hear the amazing music!</strong><br><br><br><strong>Credits</strong><br>Programming, design, updating: Gabe (GH @Archonic944)<br>Music, SFX, Art: Toft (Spotify @whoistoft)<br>Art: Cindy (???)<br>Design, Art: Luke (???)<br><strong>Created for Counterspell 2024</strong>");
+        explanation.setInnerHTML("Clear 10 waves of enemies as fast as possible! <strong>The current record is 36 seconds.</strong> <br> Use A and D to move, W and S to switch gravity (but you barely need to do that), and the arrow keys to shoot.<br><br>Shoot as much as you want, but be careful not to get hit by your own bullets...<br><br><strong>Please enable autoplay in your browser settings to hear the amazing music!</strong><br><br><br><strong>Credits</strong><br>Programming, design, updating: Gabe (GH @Archonic944)<br>Music, SFX, Art: Toft (Spotify @whoistoft)<br>Art: Cindy (???)<br>Design, Art: Luke (???)<br><strong>Created for Counterspell 2024</strong>");
         //insert paragraph after the canvas
         body.insertAfter(explanation, getRootPanel().getElement());
     }
